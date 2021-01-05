6fde58f882f2dd936df68b33aa8a296f52f74026
yanglele <2715752949@qq.com>
2021-01-05 16:34:36 +0800

    杨乐乐提交跨域配置

--- toolkit/src/main/java/com/hdd/toolkit/controller/UserController.java
+++ toolkit/src/main/java/com/hdd/toolkit/controller/UserController.java
@@ -2,6 +2,8 @@ package com.hdd.toolkit.controller;
 
 import com.hdd.toolkit.model.StatusResult;
 import com.hdd.toolkit.model.User;
+import com.hdd.toolkit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -10,16 +12,20 @@ import java.util.Map;
 /**
  * 用户
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/index")
 public class UserController {
 
+    @Autowired
+    UserService userServiceImpl;
     /**
      * 用户个人信息
      * @return
      */
     @RequestMapping("/usercenter")
-    public Map<String,Object> userCenter(){
+    public Map<String,Object> userCenter(@RequestBody Map<String , Object> result){
+        System.out.println(result);
         Map<String,Object> map = new HashMap<>();
 
         return map;
