e1ed39aae5b20deae29c0cf9c472fd26a852b729
sun0521888 <1415825115@qq.com>
2021-02-04 14:31:29 +0800

    websocket

---
+++ toolkit/src/main/java/com/hdd/toolkit/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.hdd.toolkit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import javax.websocket.server.ServerEndpointConfig;
+
+@Configuration
+public class WebSocketConfig extends ServerEndpointConfig.Configurator{
+
+    /**
+     * 注入一个ServerEndpointExporter,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
