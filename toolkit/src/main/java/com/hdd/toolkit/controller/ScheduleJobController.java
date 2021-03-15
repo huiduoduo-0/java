e9f051545d071fa399a6400c1179f2bc0610503d
yanglele <2715752949@qq.com>
2021-03-15 16:41:43 +0800

    yll

--- toolkit/src/main/java/com/hdd/toolkit/controller/ScheduleJobController.java
+++ toolkit/src/main/java/com/hdd/toolkit/controller/ScheduleJobController.java
@@ -2,10 +2,14 @@ package com.hdd.toolkit.controller;
 
 import com.hdd.toolkit.model.ScheduleJob;
 import com.hdd.toolkit.service.ScheduleJobService;
+import com.hdd.toolkit.utils.RandomValidateCode;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * (ScheduleJob)表控制层
@@ -31,4 +35,28 @@ public class ScheduleJobController {
         return "成功";
     }
 
+    @RestController
+    @RequestMapping("/code")
+    public static class
+    SafeCodeController {
+        /**
+         * 验证码
+         *
+         * @param request
+         * @param response
+         */
+        @RequestMapping(value = "/img", method = RequestMethod.GET)
+        public void image(HttpServletRequest request, HttpServletResponse response) {
+            response.setContentType("image/jpeg");// 设置相应类型,告诉浏览器输出的内容为图片
+            response.setHeader("Pragma", "No-cache");// 设置响应头信息，告诉浏览器不要缓存此内容
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expire", 0);
+            RandomValidateCode randomValidateCode = new RandomValidateCode();
+            try {
+                randomValidateCode.getValidateCode(request, response);// 输出图片方法
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
